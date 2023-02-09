73b630235b255d636da1c482bafbd853a2ff9ebd
Jihyeon Seong <dev.seongjihyeon@gmail.com>
2023-02-09 23:00:51 +0900

    Add Post domain,repository

---
+++ src/main/java/com/example/blog/domain/post/Post.java
@@ -0,0 +1,30 @@
+package com.example.blog.domain.post;
+
+import com.example.blog.domain.BaseTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
